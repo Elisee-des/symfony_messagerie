0f0e944948586c60803861ee38244dacc23e6aa9
Elisee-des <esabidani@gmail.com>
2022-04-27 10:32:56 +0000

    OK pour twig et controller debut de methode SEND

--- src/Controller/MessagesController.php
+++ src/Controller/MessagesController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Messages;
+use App\Form\MessagesType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,6 +18,26 @@ class MessagesController extends AbstractController
     {
         return $this->render('messages/index.html.twig', [
             'controller_name' => 'MessagesController',
+        ]);
+    }
+
+    /**
+     * @Route("/send", name="send")
+     */
+    public function send(Request $request): Response
+    {
+        $message = new Messages();
+
+        $form = $this->createForm(MessagesType::class, $message);
+
+        // $form->handleRequest($request);
+        
+        // if ($form->isSubmitted() && $form->isValid()) { 
+            
+        // }
+
+        return $this->render('messages/send.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
