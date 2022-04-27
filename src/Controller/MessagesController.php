bae3f996b0ab624274aec21edaf21adc6bdf7d8a
Elisee-des <esabidani@gmail.com>
2022-04-27 11:15:30 +0000

    Controller SEND et RECEIVED terminer avec success et tres simple

--- src/Controller/MessagesController.php
+++ src/Controller/MessagesController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Messages;
 use App\Form\MessagesType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -24,20 +25,40 @@ class MessagesController extends AbstractController
     /**
      * @Route("/send", name="send")
      */
-    public function send(Request $request): Response
+    public function send(Request $request, ManagerRegistry $managerRegi): Response
     {
         $message = new Messages();
 
         $form = $this->createForm(MessagesType::class, $message);
 
-        // $form->handleRequest($request);
-        
-        // if ($form->isSubmitted() && $form->isValid()) { 
-            
-        // }
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $message->setSender($this->getUser());
+
+            $em = $managerRegi->getManager();
+            $em->persist($message);
+            $em->flush();
+
+            $this->addFlash(
+                'message',
+                'Message envoyez avec success'
+            );
+
+            return $this->redirectToRoute('messages');
+        }
 
         return $this->render('messages/send.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/received", name="received")
+     */
+    public function received(): Response
+    {
+        return $this->render('messages/received.html.twig');
     }
 }
