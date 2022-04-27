7d2174e613bfc001affa0990e659bfb84f4fc1b4
Elisee-des <esabidani@gmail.com>
2022-04-27 12:55:58 +0000

    Fin avec success

--- src/Controller/MessagesController.php
+++ src/Controller/MessagesController.php
@@ -6,6 +6,7 @@ use App\Entity\Messages;
 use App\Form\MessagesType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -63,6 +64,14 @@ class MessagesController extends AbstractController
     }
 
     /**
+     * @Route("/sent", name="sent")
+     */
+    public function sent(): Response
+    {
+        return $this->render('messages/sent.html.twig');
+    }
+
+    /**
      * @Route("/read/{id}", name="read")
      */
     public function read(Messages $message, ManagerRegistry $managerRegi): Response
@@ -73,5 +82,17 @@ class MessagesController extends AbstractController
         $em->persist($message);
         $em->flush();
         return $this->render('messages/read.html.twig', compact("message"));
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     */
+    public function delete(Messages $message, ManagerRegistry $managerRegi): RedirectResponse
+    {
+        $em = $managerRegi->getManager();
+        $em->remove($message);
+        $em->flush();
+
+        return $this->redirectToRoute('received');
     }
 }
